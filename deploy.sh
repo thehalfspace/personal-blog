b3b77a95c94009cca53f1470052d85b84cbc4b63
Prithvi Thakur <thehalfspace@protonmail.com>
2021-01-01 23:53:08 +0530

    rename blog to posts

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo 
-# hugo -t hugo-coder # if using a theme, replace with `hugo -t <YOURTHEME>`
+# hugo
+hugo -t hugo-coder # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
